1f0aa860d20d676a4dbe786425780950fcef29ba
alleizon <112495304+alleizon@users.noreply.github.com>
2023-03-14 19:20:51 +0200

    refactored field components to render Input components on click

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,12 +1,12 @@
 import { Component } from "react";
 import Input from "./utils/Input";
 
-export default class Title extends Component {
+export default class Name extends Component {
   constructor() {
     super();
     this.state = {
       showInput: false,
-      title: "Your title here",
+      name: "Your name here",
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleKeyUp = this.handleKeyUp.bind(this);
@@ -14,7 +14,7 @@ export default class Title extends Component {
 
   handleKeyUp(e, text) {
     if (e.key !== "Enter") return;
-    this.setState({ showInput: false, title: text ? text : "Your title here" });
+    this.setState({ showInput: false, name: text ? text : "Your name here" });
   }
 
   handleClick() {
@@ -25,9 +25,9 @@ export default class Title extends Component {
 
   render() {
     return this.state.showInput ? (
-      <Input value={this.state.title} handleKeyUp={this.handleKeyUp} />
+      <Input value={this.state.name} handleKeyUp={this.handleKeyUp} />
     ) : (
-      <p onClick={this.handleClick}>{this.state.title}</p>
+      <h1 onClick={this.handleClick}>{this.state.name}</h1>
     );
   }
 }
---
+++ src/components/utils/Input.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+export default class Input extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: this.props.value,
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  render() {
+    return (
+      <input
+        className="border border-black"
+        type="text"
+        value={this.state.text}
+        onInput={this.handleInput}
+        onKeyUp={(e) => {
+          this.props.handleKeyUp(e, this.state.text);
+        }}
+        autoFocus
+      ></input>
+    );
+  }
+}
